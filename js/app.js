3c8aa387715558399bd10063daa67a56bf0f8e87
Andres Mills Gallego <geneseewoodworking@gmail.com>
2021-12-29 21:08:13 -0800

    Final touches on the JS

--- js/app.js
+++ js/app.js
@@ -2,8 +2,9 @@
 'use strict';
 console.log('Anyone else think "salmon cookies" sound gross?');
 
-let storeHoursAndTotal = ['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00pm','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm','7:00pm', 'Daily Location Total'];
-let storeHoursOnly = ['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00pm','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm','7:00pm'];
+let storeHoursAndTotal = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', 'Daily Location Total'];
+let storeHoursOnly = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
+let controlCurvePercentage = [0.5, 0.75, 1.0, 0.6, 0.8, 1.0, 0.7, 0.4, 0.6, 0.9, 0.7, 0.5, 0.3, 0.4];
 let storeArray = [];
 let hourlyTotalsArray = [];
 
@@ -11,7 +12,7 @@ const storeTable = document.querySelector('table tbody');
 const tossersHeader = document.getElementById('cookieTossers');
 const tossersBody = document.getElementById('t2Body');
 
-function Store(name,min, max, avg) {
+function Store(name, min, max, avg) {
   this.storeName = name;
   this.minCust = min;
   this.maxCust = max;
@@ -20,10 +21,10 @@ function Store(name,min, max, avg) {
   this.cookieTotal = 0;
   this.customersPerHour = [];
   this.cookieTossersPerHour = [];
-  this.randomCustPerHour = function() {
-    for (let i = 0; i < storeHoursAndTotal.length - 1; i++) {
+  this.randomCustPerHour = function () {
+    for (let i = 0; i < storeHoursOnly.length; i++) {
       let hourlyCustomer = (Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust));
-      //console.log(hourlyCustomer);//To check the method is working
+      hourlyCustomer *= controlCurvePercentage[i];
       this.customersPerHour.push(hourlyCustomer);
       let hourlyCookieTotal = Math.ceil(hourlyCustomer * this.avgCookiePerCust);
       this.totalCookiesPerHour.push(hourlyCookieTotal);
@@ -31,7 +32,7 @@ function Store(name,min, max, avg) {
     }
     this.totalCookiesPerHour.push(this.cookieTotal);
   };
-  this.renderTable = function(variable,array) {
+  this.renderTable = function (variable, array) {
     this.randomCustPerHour();
     let tr = document.createElement('tr');
     variable.appendChild(tr);
@@ -45,13 +46,13 @@ function Store(name,min, max, avg) {
       tr.appendChild(td);
     }
   };
-  this.tossersNeeded = function() {
+  this.tossersNeeded = function () {
     for (let i = 0; i < this.customersPerHour.length; i++) {
       let cookieTossers = 2;
       if (this.customersPerHour[i] > 40 && this.customersPerHour[i] < 60) {
         cookieTossers += 1;
         this.cookieTossersPerHour.push(cookieTossers);
-      } else if ( this.customersPerHour[i] > 60) {
+      } else if (this.customersPerHour[i] > 60) {
         let largeCustomerFormula = Math.ceil(this.customersPerHour[i] / 20) - 2;
         cookieTossers += largeCustomerFormula;
         this.cookieTossersPerHour.push(cookieTossers);
@@ -60,17 +61,15 @@ function Store(name,min, max, avg) {
       }
     }
   };
-
-
   storeArray.push(this);
 }
-let seattleStore = new Store('Seattle',23,65,6.3);
-let tokyoStore = new Store('Tokyo',3,24,1.2);
-let dubaiStore = new Store('Dubai',11,38,3.7);
-let parisStore = new Store('Paris',20,38,2.3);
-let limaStore = new Store('Lima',2,16,4.6);
+let seattleStore = new Store('Seattle', 23, 65, 6.3);
+let tokyoStore = new Store('Tokyo', 3, 24, 1.2);
+let dubaiStore = new Store('Dubai', 11, 38, 3.7);
+let parisStore = new Store('Paris', 20, 38, 2.3);
+let limaStore = new Store('Lima', 2, 16, 4.6);
 
-let tableHeader = function(id,array) {
+let tableHeader = function (id, array) {
   const tHead = document.getElementById(id);
   let tr = document.createElement('tr');
   tHead.appendChild(tr);
@@ -83,16 +82,16 @@ let tableHeader = function(id,array) {
   }
 };
 
-let tableFooter = function() {
+let tableFooter = function () {
   const tableFoot = document.querySelector('table tfoot');
   let tr = document.createElement('tr');
   tableFoot.appendChild(tr);
   let th = document.createElement('th');
   tr.appendChild(th);
   th.textContent = 'Totals';
-  for (let j =0; j < storeHoursAndTotal.length; j++) {
+  for (let j = 0; j < storeHoursAndTotal.length; j++) {
     let hourlyTotal = 0;
-    for (let i =0; i < storeArray.length; i++) {
+    for (let i = 0; i < storeArray.length; i++) {
       hourlyTotal += storeArray[i].totalCookiesPerHour[j];
     } hourlyTotalsArray.push(hourlyTotal);
   }
@@ -102,28 +101,24 @@ let tableFooter = function() {
     td.textContent = hourlyTotalsArray[i];
     tr.appendChild(td);
   }
-  let grandCookieTotal = 0;
-  for (let i = 0; i < storeArray.length; i++) {
-    grandCookieTotal += storeArray[i].cookieTotal;
-  }
 };
 
 
 // Everything gets called and rendered with the code below
 
-tableHeader('salesData',storeHoursAndTotal);
+tableHeader('salesData', storeHoursAndTotal);
 
 for (let i = 0; i < storeArray.length; i++) {
-  storeArray[i].renderTable(storeTable,storeArray[i].totalCookiesPerHour);
+  storeArray[i].renderTable(storeTable, storeArray[i].totalCookiesPerHour);
 }
 
 tableFooter();
 
-tableHeader('cookieTossers',storeHoursOnly);
+tableHeader('cookieTossers', storeHoursOnly);
 
 for (let i = 0; i < storeArray.length; i++) {
   storeArray[i].tossersNeeded();
-  storeArray[i].renderTable(tossersBody,storeArray[i].cookieTossersPerHour);
+  storeArray[i].renderTable(tossersBody, storeArray[i].cookieTossersPerHour);
 }
 
 
