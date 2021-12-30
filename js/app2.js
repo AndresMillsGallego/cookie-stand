5cb7a250c4d0d26408f5095e018b056a9f1f7790
Andres Mills Gallego <geneseewoodworking@gmail.com>
2021-12-30 14:22:05 -0800

    Finished lab 07 with stretch goals

--- js/app2.js
+++ js/app2.js
@@ -2,9 +2,9 @@
 console.log('Salut!');
 
 let storeHoursOnly = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
-let locations = ['Seattle','Tokyo','Dubai','Paris','Lima'];
+let locations = ['Seattle', 'Tokyo', 'Dubai', 'Paris', 'Lima'];
 
-let renderLocations = function() {
+let renderLocations = function () {
   const firstDiv = document.getElementById('div1');
   let ul = document.createElement('ul');
   firstDiv.appendChild(ul);
@@ -19,11 +19,15 @@ let renderLocations = function() {
 };
 renderLocations();
 
-let storeHours = function() {
+let storeHours = function () {
   const thirdDiv = document.getElementById('div3');
   let h3 = document.createElement('h3');
   h3.textContent = 'Our Store Hours';
   thirdDiv.appendChild(h3);
+  let days = document.createElement('article');
+  days.setAttribute('id', 'days');
+  days.textContent = 'Monday - Friday';
+  thirdDiv.appendChild(days);
   let hours = document.createElement('article');
   hours.textContent = `${storeHoursOnly[0]} - ${storeHoursOnly[13]}`;
   thirdDiv.appendChild(hours);
